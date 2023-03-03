0dbee531630b2ee65e4fca882b0e4d1e1b0ba343
Duke145 <Duk145@yandex.ru>
2023-03-03 23:17:49 +0300

    add Stars

---
+++ src/Module1/Stars.java
@@ -0,0 +1,21 @@
+package Module1;
+
+import java.util.Scanner;
+
+public class Stars {
+    public static void main(String[] args) throws Exception {
+        try (Scanner scanner = new Scanner(System.in)) {
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+            String template = scanner.next();
+            if (n>0 && m>0) {
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < m; j++) {
+                        System.out.print((j == (m - 1)) ? template : template + " ");
+                    }
+                    System.out.println();
+                }
+            } else System.out.println("Некорректно введены данные");
+        }
+    }
+}
