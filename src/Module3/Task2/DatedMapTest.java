8df28dcdfddd49a2834c49a28f3fe11d8b26da39
Duke145 <Duk145@yandex.ru>
2023-03-18 01:39:59 +0300

    add DatedMap task

---
+++ src/Module3/Task2/DatedMapTest.java
@@ -0,0 +1,27 @@
+package Module3.Task2;
+
+public class DatedMapTest {
+    public static void main(String[] args) {
+        DatedMap map = new DatedMapImpl();
+        map.put("first","asd");
+        map.put("second","pasdasd");
+        System.out.println(map.get("first"));
+        System.out.println(map.get("second"));
+        System.out.println(map.get("third"));
+        System.out.println(map.containsKey("first"));
+        System.out.println(map.containsKey("second"));
+        System.out.println(map.containsKey("third"));
+        System.out.println(map.keySet());
+        System.out.println(map.getKeyLastInsertionDate("first").getTime());
+        System.out.println(map.getKeyLastInsertionDate("second"));
+        System.out.println(map.getKeyLastInsertionDate("third"));
+        map.remove("first");
+        map.put("third","111123123");
+        System.out.println(map.getKeyLastInsertionDate("first"));
+        System.out.println(map.getKeyLastInsertionDate("second"));
+        System.out.println(map.getKeyLastInsertionDate("third"));
+        map.put("first","asd");
+        System.out.println(map.getKeyLastInsertionDate("first").getTime());
+
+    }
+}
